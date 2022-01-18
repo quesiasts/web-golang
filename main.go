9e23f6a22dca312e95cf5e5f3bafa990a125a1cd
Quesia Santos <quesia.santos@olist.com>
2022-01-18 18:55:51 -0300

    add sctruct inital

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tênis", "Confortável", 100, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Blusa", "Florida", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
